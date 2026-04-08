c358441db99099003fbca946140ab8ef36b2d3cf
Dmitry Vakhnenko <dimaeatsanapple@gmail.com>
2026-04-08 18:42:02 +0300

    chore: add product-catalog

---
+++ src/components/ProductCatalog.vue
@@ -0,0 +1,15 @@
+<template>
+	<main class="product-catalog">
+		<slot />
+	</main>
+</template>
+
+<style lang="css">
+.product-catalog {
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	gap: 24px;
+
+	padding-inline: var(--page-container-padding-inline);
+}
+</style>
